4a94373a2229de81c921cec39ccf70486b56baad
Adesh <shambharkaradesh9@gmail.com>
2021-03-04 20:20:57 +0530

    min function with second test

--- src/min.spec.js
+++ src/min.spec.js
@@ -6,4 +6,8 @@ describe('min', () => {
         expect(min([1],0)).toEqual(1);
     });
   
+    it('min of [6,9,7,4,5] is 4', () => {
+        expect(min([6,9,7,4,5],0)).toEqual(4);
+    });
+    
 })
