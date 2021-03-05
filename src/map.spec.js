b606688e177c18a5459e92586c5955422cc6cba8
Adesh <shambharkaradesh9@gmail.com>
2021-03-05 20:18:57 +0530

    map function with second test of identity function

--- src/map.spec.js
+++ src/map.spec.js
@@ -6,4 +6,8 @@ describe('map', () => {
         expect(map([],"identity")).toEqual([]);
     });
     
+    it('map([1,2,3],identity) is [1,2,3]', () => {
+        expect(map([1,2,3],"identity")).toEqual([1,2,3]);
+    });
+
 })
